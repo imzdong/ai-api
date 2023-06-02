fb966b7f4af71f7c5810dbc09423a70feb812b3d
dong.zhou <dong.zhou@edetek.com>
2023-06-02 15:52:46 +0800

    mongodb配置

--- src/main/java/org/imzdong/ai/config/MongodbConfig.java
+++ src/main/java/org/imzdong/ai/config/MongodbConfig.java
@@ -2,13 +2,15 @@ package org.imzdong.ai.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MongoDBConfig {
+public class MongodbConfig {
     
-    private String uri = "xx";
+    @Value("${cloud.mongodb.url}")
+    private String uri;
 
     @Bean
     public MongoClient initClient(){
